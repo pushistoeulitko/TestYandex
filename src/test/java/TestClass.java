9a6ac19439824bf2ec2554f0d0a34cc8e1fff65a
pushistoeulitko <pushistoeulitko@yandex.ru>
2020-07-27 12:12:16 +0500

    new

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,8 +1,10 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 import java.util.Iterator;
 import java.util.List;
@@ -42,34 +44,33 @@ public class TestClass {
         driver.findElement(By.xpath(name)).click();
     }
 
-    @Test (description ="Проверка и выставление русского языка")
+    @Test(description = "Проверка и выставление русского языка")
     public void switchOverLanguageTestRu() {
         clickItem("//button[contains(@class,'mail-SettingsButton')]");
         clickItem("//a[@class='mail-SettingsPopup__title']/span");
 
         String languageRu = driver.findElement(By.xpath("//span[@class='b-selink__link mail-Settings-Lang']")).getText();
-        if (languageRu.equals("Русский")) {
-            System.out.println("Выставлен русский язык");
-        } else {
+        if (! languageRu.equals("Русский")) {
             clickItem("//span[contains(@class,'mail-Settings-Lang_arrow')]");
             clickItem("//a[@data-params='lang=ru']");
-            System.out.println("Изменен на русский язык");
         }
+        String languageRu1 = driver.findElement(By.xpath("//span[@class='b-selink__link mail-Settings-Lang']")).getText();
+        Assert.assertEquals(languageRu1, "Русский");
     }
 
-    @Test (description ="Проверка и выставление английского языка")
+    @Test(description = "Проверка и выставление английского языка")
     public void switchOverLanguageTestEn() {
         clickItem("//button[contains(@class,'mail-SettingsButton')]");
         clickItem("//a[@class='mail-SettingsPopup__title']/span");
 
         String languageEn = driver.findElement(By.xpath("//span[@class='b-selink__link mail-Settings-Lang']")).getText();
-        if (languageEn.equals("English")) {
-            System.out.println("Выставлен English язык");
-        } else {
+        if (! languageEn.equals("English")) {
             clickItem("//span[contains(@class,'mail-Settings-Lang_arrow')]");
             clickItem("//a[@data-params='lang=en']");
-            System.out.println("Изменен на English");
         }
+
+        String languageEn1 = driver.findElement(By.xpath("//span[@class='b-selink__link mail-Settings-Lang']")).getText();
+        Assert.assertEquals(languageEn1, "English");
     }
 
     @Test (description ="Удалить сообщение клавишей делит в класиватуры")
@@ -80,32 +81,29 @@ public class TestClass {
         }
         driver.findElement(By.xpath("//input[@class='textinput__control']")).sendKeys(Keys.DELETE);
     }
-      //"//div[contains(@class,'mail-Toolbar-Item_main-select-all')]//span[@class='checkbox_view']"
-       // List<WebElement> elem = driver.findElements(By.xpath("//span[@class='_nb-checkbox-flag _nb-checkbox-normal-flag']"));
-       // for (WebElement webElement : elem) {
-         //   webElement.click();
-       // }
-//        driver.findElement(By.xpath("//span[@class='_nb-checkbox-flag _nb-checkbox-normal-flag']"));
-       // driver.findElement(By.xpath("//div[contains(@class,'js-layout-aside-inner-box')]")).sendKeys(Keys.DELETE);
-        //clickItem("//button/span[@class='_nb-button-content']");
-       // clickItem("//input[@id='nb-checkbox_0']/following-sibling::span[contains(@class,'_nb-checkbox-normal-flag')]");
-        //driver.findElement(By.xpath("//input[@id='nb-checkbox_0']/following-sibling::span[contains(@class,'_nb-checkbox-normal-flag')]")).sendKeys(Keys.DELETE);
-
 
     @Test (description ="Удалить сообщение кликнув на значок удалить")
     public void deletingMessagesTestWithClick() {
        //по одному
        // clickItem("//input[@id='nb-checkbox_0']/following-sibling::span[contains(@class,'_nb-checkbox-normal-flag')]");
+        String x = driver.findElement(By.xpath("//span[@class='mail-NestedList-Item-Info-Link-Text']")).getText();
         List<WebElement> elem = driver.findElements(By.xpath("//span[@class='_nb-checkbox-flag _nb-checkbox-normal-flag']"));
         elem.forEach(WebElement::click);
-        clickItem("//span[contains(@class,'js-toolbar-item-title-delete')]");
+        //clickItem("//span[contains(@class,'js-toolbar-item-title-delete')]");
+        clickItem("//div[contains(@data-key, 'delete')]");
         // clickItem("//button/span[@class='_nb-button-content']"); добавлять еще сообщения
+        String y = driver.findElement(By.xpath("//span[@class='mail-NestedList-Item-Info-Link-Text']")).getText();
+        System.out.println(x + y);
+        Assert.assertNotEquals(x, y);
     }
 
     @Test (description ="Попытка удалить сообщения не выделяя чекбоксы")
     public void noDeletingMessagesTestWithClick() {
+        String x = driver.findElement(By.xpath("//span[@class='mail-NestedList-Item-Info-Link-Text']")).getText();
         clickItem("//span[contains(@class,'js-toolbar-item-title-delete')]");
-
+        String y = driver.findElement(By.xpath("//span[@class='mail-NestedList-Item-Info-Link-Text']")).getText();
+       // System.out.println(x + " y");
+        Assert.assertEquals(x, y);
     }
 
     @Test (description = "Отправка сообщения себе")
@@ -113,32 +111,33 @@ public class TestClass {
         clickItem("//a[contains(@class,'js-main-action-compose')]");
         driver.findElement(By.xpath("//div[contains(@class,'tst-field-to')]//div[@class='composeYabbles']")).sendKeys("pushist0eulitko@yandex.ru");
         clickItem("//div/button[contains(@class,'ComposeControlPanelButton-Button_action')]");
-        //String message = driver.findElement(By.xpath("//*[@id='nb-1']/body/div[13]/div/div[1]/div[1]/span")).getText();
-        //if (message.equals("Письмо отправлено")) {
-           // System.out.println("Сообщение отправлено");
-        //}
-
+//алерт не дает перейти сразу
+        clickItem("//a[@href='#sent']/span[@class='mail-NestedList-Item-Name']");//тыкнуть на отправленные
+        String n = driver.findElement(By.xpath("//a[@href='#sent']/span[@class='mail-NestedList-Item-Info-Extras']")).getText(); //количество отправленных
+        Assert.assertNotEquals(n, n);
+        //проверка на не равно не исключает удаление
     }
-    @Test (description ="Отправка сообщения по неверному адресу")
+
+    @Test(description = "Отправка сообщения по неверному адресу")
     public void sendingMessageFalseEmail() {
         clickItem("//a[contains(@class,'js-main-action-compose')]");
-        WebElement sent = driver.findElement(By.xpath("//div[contains(@class,'tst-field-to')]//div[@class='composeYabbles']"));
-        sent.sendKeys("fxjfsjf");
+        driver.findElement(By.xpath("//div[contains(@class,'tst-field-to')]//div[@class='composeYabbles']")).sendKeys("fxjfsjf");
         clickItem("//div/button[contains(@class,'ComposeControlPanelButton-Button_action')]");
-        //String message =clickItem("//div[@class='ComposeConfirmPopup-Title']/span")).getText();
-       // if (message.equals("Проверьте получателя")) {
-        //    System.out.println("Сообщение не отправлено");
-        }
+        String message = driver.findElement(By.xpath("//div[@class='ComposeConfirmPopup-Title']/span")).getText();
+        Assert.assertEquals(message, "Проверьте получателя");
+    }
 
     @Test (description ="Отправка сообщения по пустому адресу")
     public void clickSendMessageButton() {
         clickItem("//a[contains(@class,'js-main-action-compose')]");
         clickItem("//div/button[contains(@class,'ComposeControlPanelButton-Button_action')]");
+        String message =driver.findElement(By.xpath("//div[@class='ComposeConfirmPopup-Title']/span")).getText();
+        Assert.assertEquals(message, "Письмо не отправлено");
     }
 
     @AfterTest
     public void exit() {
-       // driver.quit();
+       driver.quit();
     }
 }
 
